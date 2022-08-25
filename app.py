d7f503a1e4cb2e6a13bf59452344b1135570644a
Khanh Phan <khanh.phan@tokyotechies.com>
2022-08-26 01:00:20 +0900

    load model

--- app.py
+++ app.py
@@ -6,7 +6,15 @@ import streamlit as st
 
 from PIL import Image
 
-@st.cache(suppress_st_warning = True)
+
+def load_model():
+    model = fasttext.train_supervised("data/train_clean.txt", lr = 0.3, epoch = 25, wordNgrams = 2)
+    print("[INFO] loaded face mask detector model")
+    #model.save_model("model.bin")
+    #model = fasttext.load_model("model.bin")
+    return model
+
+
 def predict_humor(model: fasttext.FastText._FastText, sentence: str) -> str:
     predictions = model.predict(sentence)
     label = predictions[0][0].split("__label__")[1]
@@ -17,8 +25,8 @@ def predict_humor(model: fasttext.FastText._FastText, sentence: str) -> str:
     confidence = predictions[1][0]
     return "{} ({:.2f}% confident)".format(label, confidence * 100)
 
-def main():
 
+def main():
     image = Image.open("laughing-emoji.jpg")
     st.image(image, use_column_width = True)
 
@@ -27,12 +35,12 @@ def main():
 
     This app leverages Facebook Research's [fastText](https://fasttext.cc/) library to predict if your sentence is humorous or not!
     """)
-
-    model = fasttext.train_supervised("data/train_clean.txt", lr = 0.3, epoch = 25, wordNgrams = 2)
-    print("To AAAA")
-    user_sentence = st.text_input("Check if your sentence is humorous or not", "I am a master of tearable puns but only on paper")
-    print("To BBBB")
+    model = load_model()
+    user_sentence = st.text_input("Check here if your sentence is humorous or not", "I am a master of tearable puns but only on paper")
+    print("user input: ", user_sentence)
     st.write("Your sentence is:", predict_humor(model, user_sentence))
-    print("To CCCC")
+
+
 if __name__ == "__main__":
     main()
+
